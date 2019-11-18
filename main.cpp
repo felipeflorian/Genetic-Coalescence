6becd7a37f7ec0a6746edd5e8a91bd4c6b39b966
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-11-18 18:50:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,19 @@ using namespace std;
 int main(){
   Simulations test;
   vector<string> change = {"1","2","3","4","5"};
-  test.set_poblation(change);
+  test.set_population(change);
 
 
   test.nodes();
   test.do_simulation();
-  vector<string> pob = test.get_poblation();
+  vector<string> pob = test.get_population();
   int sz = pob.size();
 
   for(int i = 0; i < sz; i++){
     cout << pob[i] << " ";
   }
   cout << endl;
+
 
   return 0;
 }
