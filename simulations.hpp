3468157e89e71ded40523f53e3b34a3698ac80c3
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-11-18 18:49:36 -0500

    Update simulations.hpp

--- simulations.hpp
+++ simulations.hpp
@@ -5,29 +5,31 @@
 #include <string>
 using namespace std;
 
+struct Node{
+  string key;
+  vector<Node *> sons;
+};
+
 class Simulations{
 private:
-  struct Node{
-    string key;
-    vector<Node *> sons;
-    vector<string> p;
-  };
   Node *parent;
   vector<Node *> roots;
-  vector<string> poblation;
+  vector<string> population;
   vector<int> count;
   void doing_nodes( vector<string> p);
   void create_count();
+  Node* find_node(string key,vector<Node *> x);
 
 public:
   Simulations(){}
   //~Simulations();
-  void set_poblation(const vector<string> nw);
-  vector<string> get_poblation();
-  void nodes() {doing_nodes(poblation);}
+  void set_population(const vector<string> nw);
+  vector<string> get_population();
+  void nodes() {doing_nodes(population);}
   void print_nodes();
   void do_simulation();//Number o iterations
-
+  Node* find(string key){return find_node(key, roots);}
+  void simulation();
 
 };
 
