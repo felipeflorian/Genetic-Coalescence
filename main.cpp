58e5aef1becfc8d554e76895134d026a0e1affee
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-11-21 16:19:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
   Simulations test;
-  vector<int> change = {1,2,3,4,5};
+  vector<int> change = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
   test.set_population(change);
 
 
@@ -16,7 +16,7 @@ int main(){
   //for(unsigned int u = 0; u < pob.size(); u++){cout << pob[u] << " ";}
   cout << endl;
   //test.doing_simulations(3);
-  test.doing_simulations(5);
+  test.doing_simulations(20);
   test.display_tree();
   //lo hace 5 veces
 
