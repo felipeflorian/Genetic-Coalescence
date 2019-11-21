c434cd01d3830157f3b6194c6cf56fc82395f68d
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-11-20 20:44:31 -0500

    Update simulations.cpp

--- simulations.cpp
+++ simulations.cpp
@@ -1,37 +1,33 @@
 #include "simulations.hpp"
-#include <iostream>
-#include <vector>
-#include <string>
-#include <stdlib.h>
-#include <time.h>
-#include <cstdlib>
-#include <stdexcept>
-#include <stdio.h>
-#include <list>
-using namespace std;
+
 
 //Private functions
-void Simulations::doing_nodes( vector<string> nw){
+void Simulations::doing_nodes( vector<int> nw){
   int sz = nw.size();
   for(int i = 0; i < sz; ++i){
-    string k = nw[i];
+    int k = nw[i];
     Node *temp;
     temp = new Node;
     temp->key = k;
-    temp->sons = nullptr;
     roots.push_back(temp);
   }
 }
 
 void Simulations::create_count(){
   int sz = population.size();
-  for(int i = 0; i < sz; i++){
-    count.push_back(0);
+  if(count.size() == 0){
+    for(int i = 0; i < sz; i++){
+      count.push_back(0);
+    }
+  }else{
+    for(int y = 0; y < sz; y++){
+      count[y] = 0;
+    }
   }
 }
 
 
-void  Simulations:: set_population(const vector<string> nw){
+void  Simulations:: set_population(const vector<int> nw){
   int sz = nw.size();
   if(population.empty()){
     for(int i = 0; i < sz;++i){
@@ -44,7 +40,7 @@ void  Simulations:: set_population(const vector<string> nw){
   }
 }
 
-vector<string> Simulations::get_population(){
+vector<int> Simulations::get_population(){
   return population;
 }
 
@@ -55,64 +51,9 @@ void Simulations::print_nodes(){
   }
 }
 
-
-
-
-// Node* Simulations::find_node(vector<Node *> &x,string key,string temp){
-//   vector<string> Keys;
-//   for(unsigned int i = 0; i < x.size();i++ ){
-//     if(x[i]->key == key){
-//       return x[i];
-//       }
-//     }
-//     char k = key.at(0);
-//     string s = "";
-//     for(int j = 0; j < key.length();j++){
-//       char r = key.at(j);
-//
-//       if( r == ':'){
-//         cout << "pushing ..." ;
-//         Keys.push_back(s);
-//         cout << s << endl;
-//         s = "";
-//       }
-//       if(r != ':'){
-//         s += r;
-//
-//       }
-//     }
-//     Keys.push_back(s);
-//     cout << endl;
-//     cout << Keys.size() << endl;
-//     for(unsigned int y = 0; y < Keys.size(); y++){
-//       cout << Keys[y] << " ";
-//     }
-//     cout << endl;
-//     string newKey = "";
-//     for(unsigned int o = 1; o < Keys.size();o++){
-//       newKey =":"+ Keys[o];
-//     }
-//     string r = Keys[0];
-//     temp += r;
-//     cout << temp << endl;
-//     cout << newKey << endl;
-//     // cout << newKey << endl;
-//     // cout << temp << endl;
-//     for(unsigned int u = 0; u < x.size(); u++){
-//       if(x[u]->key == temp){
-//         // cout << "holi " << endl;
-//         // cout << x[u]->key << endl;
-//         return x[u];
-//       }
-//     }
-//     return x[0];
-//
-//   return nullptr;
-// }
-
-void create_sons(int n, string s, vector<Node *> roots){
+void Simulations::create_sons(int n, int s, vector<Node *>  roots){
   Node * copy;
-  for(unsigned int i = 0; i < roots.size(); i++){
+  for(unsigned int i = 0; i < population.size(); i++){
     if(roots[i]->key == s){
       copy = roots[i];
       break;
@@ -121,52 +62,156 @@ void create_sons(int n, string s, vector<Node *> roots){
 
   vector<Node *> find_son;
   find_son = copy->sons;
-  if(find_son == nullptr){
+  if(find_son.size()==0){
     for(int j = 0; j < n; j++){
       Node *nw;
+      nw = new Node;
       nw->key = s;
-      nw->sons = nullptr;
-      copy.push_back(nw);
+      copy->sons.push_back(nw);
     }
   }else{
     for(unsigned int p = 0; p < find_son.size(); p++){
-      if(find_son[i]->sons == nullptr){
-        for(int j = 0; j < n; j++){
-          Node *nw;
-          nw->key = s;
-          nw->sons = nullptr;
-          copy.push_back(nw);
+      Node *e = copy->sons[p];
+      if(e->sons.size() == 0){
+        if(n%2 == 0){
+          for(int j = 0; j < n; j++){
+            Node *nw;
+            nw = new Node;
+            nw->key = s;
+            e->sons.push_back(nw);
+          }
+        }else{
+          for(int j = 0; j <= n; j++){
+            Node *nw;
+            nw = new Node;
+            nw->key = s;
+            e->sons.push_back(nw);
+          }
         }
       }else{
-        Node *x;
-        vector<Node *> copy2;
-        copy2 = x->sons;
-        create_sons(n, s, copy2);
+          cout << "entre al else " << endl;
+          int sz = copy->sons.size();
+          for(int i = 0; i < sz; i++){
+            Node *temp;
+            temp = copy->sons[i];
+            Node *temp2 = nullptr;
+            while(temp->sons.size() != 0){
+              create_sons(n,s temp->sons);
+            }
+          }
       }
     }
   }
 
 }
 
-void Simulations:: simulation(int k){
-  do_simulation();
-  for(int i = 0; i < k -1 ; i++){
 
+void Simulations::do_simulation(){
+  int sz = population.size();
+  vector<int> newpopulation;
+  if(roots.empty())
+    throw runtime_error(" No roots :( ");
+  else{
+    create_count();
+    for(int i = 0; i < sz; i++){
+      int ran = 1 + rand() % (sz-1);
+      cout << ran << " ";
+      count[ran-1]++;
+    }
+    int x = count.size();
+    cout << endl;
+    for(int i = 0; i < x ; i++){
+      cout << count[i] << " ";
+    }
+    cout << endl;
+    for(int j = 0; j < x; j++){
+      if(count[j]>=1){
+        int son = j+1;
+        for(int k = 1; k<= count[j];k++){
+          newpopulation.push_back(son);
+        }
+      }
+    }
+
+  set_population(newpopulation);
+  for(unsigned int k = 0; k <population.size();k++){cout << population[k] << " ";}
+  }
+  cout << endl;
+  for(int r = 0; r < sz; r++){
+    int x = r+1;
+    create_sons(count[r],x,roots);
   }
 }
 
+void Simulations:: doing_simulations(int k){
 
-// string Simulations:: take_last(Node * &t){
-//   string temp = t->key;
-//   string final = "";
-//   for(int j = 0; j < temp.length();j++){
-//     char p = temp.at(j);
-//     final += p;
-//     cout << final << endl;
-//     if(p == ':'){
-//       final = "";
-//     }
-//   }
-//   cout << final << endl;
-//   return final;
-// }
+  do_simulation();
+  cout << endl;
+  int sz = population.size();
+  for(int i = 0; i < k -1 ; i++){
+    cout << "doing simulation " << endl;
+    vector<int> newpopulation(sz,0);
+    create_count();
+    for(int i = 0; i < sz; i++){
+      int ran = 1 + rand() % (sz-1);
+      cout << ran << " ";
+      count[ran-1]++;
+    }
+    int x = count.size();
+    cout << endl;
+    cout << "Printing count ..." << endl;
+    for(int i = 0; i < x ; i++){
+      cout << count[i] << " ";
+    }
+    cout << endl;
+    int count2 = 0;
+    for(int j = 0; j < x; j++){
+      if(count[j] >= 1){
+        for(int f = 0; f < count[j]; f++){
+          cout << population[j] << " ";
+          newpopulation[count2] = population[j];
+          count2++;
+        }
+      }
+    }
+    cout << "printing population" << endl;
+    set_population(newpopulation);
+    for(unsigned int k = 0; k <population.size();k++){cout << population[k] << " ";}
+    cout << endl;
+    cout << "creating sons " << endl;
+    for(int r = 0; r <sz ;r++){
+      int x = population[r];
+      create_sons(count[r],x,roots);
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
+
+void Simulations::display(Node *root){
+  queue<Node *> q;
+  q.push(root);
+  while(!q.empty()){
+    int n = q.size();
+    while(n>0){
+      Node *p = q.front();
+      q.pop();
+      cout << p->key << " ";
+
+      for(unsigned int i = 0; i < p->sons.size(); i++){
+        q.push(p->sons[i]);
+      }
+      n--;
+    }
+    cout << endl;
+  }
+  cout << endl << endl;
+}
+
+void Simulations:: display_tree(){
+  for(unsigned int i = 0; i < roots.size(); i++){
+    display(roots[i]);
+  }
+
+
+}
