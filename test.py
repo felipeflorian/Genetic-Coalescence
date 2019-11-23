5659b3910eeea015137531e9ad72a061f69a17ba
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-11-23 15:10:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -37,26 +37,101 @@ print("Formula: ", e)
 
 
 
+l = s
+k = str(s)
+e = ""
+for f in range(3):
+    e += k[f]
+k = float(e)
+G = nx.Graph()
+for i in range(10):
+    G.add_node(i, pos = (s,s))
+    s -= 4*k
 
-# G = nx.Graph()
-# #parent
-# G.add_node(0, pos = (1000,1000))
-#
-# print(int(s))
-# for i in range(1,int(r)):
-#     G.add_node(i,pos=(10*(i+1),10*(i+1)))
-#
-#
-#
-# # pos2 = nx.get_node_attributes(P, 'pos')
-# #         weight2 = nx.get_edge_attributes(P,'weight')
-# #
-# #         plt.figure()
-# #
-# #         nx.draw_networkx(P,pos2,nodelist=nodesP,node_color= color)
-# #         nx.draw_networkx_edge_labels(P,pos2,edge_labels = weight2)
-# #         plt.show()
-#
-# pos = nx.get_node_attributes(G, 'pos')
-# nx.draw(G,pos, with_labels = True)
-# plt.show()
+x = G.nodes()
+for i in range(len(x)):
+    if i + 1 < len(x):
+        G.add_edge(i,i+1,weight = k)
+
+
+
+pos = nx.get_node_attributes(G, 'pos')
+
+
+n = []
+
+for i in pos.values():
+    n.append(i)
+print(n)
+for i in range(10,19):
+    x = n[i-9]
+    c = -x[0]
+    d = x[1]
+    G.add_node(i,pos = (c,d))
+
+r = G.nodes()
+
+G.add_edge(0,10,weight = k)
+
+for i in range(10,18):
+    if i + 1 <= len(r):
+        G.add_edge(i,i+1,weight = k)
+
+pos3 = nx.get_node_attributes(G, 'pos')
+
+
+new = []
+for i in range(19,27):
+    new.append(i)
+
+count = 1
+for i in range(len(new)):
+    x = n[len(n)-count]
+    count += 1
+    d = x[0]
+    t = x[1]
+    G.add_node(new[i], pos =(d/2,t))
+
+
+
+
+new2 = []
+for i in range(28,36):
+    new2.append(i)
+
+n2 = []
+for i in pos3.values():
+    n2.append(i)
+count2 = 1
+print(n2)
+
+for i in range(len(new2)):
+    x = n2[len(n2)-(i+1)]
+    print(x)
+    count += 1
+    d = x[0]
+    t = x[1]
+    G.add_node(new2[i],pos=(d/2,t))
+
+G.add_edge(1,26,weight=k)
+G.add_edge(2,25,weight=k)
+G.add_edge(3,24,weight=k)
+G.add_edge(4,23,weight=k)
+G.add_edge(5,22,weight=k)
+G.add_edge(6,21,weight=k)
+G.add_edge(7,20,weight=k)
+G.add_edge(8,19,weight=k)
+G.add_edge(28,17,weight=k)
+G.add_edge(29,16,weight=k)
+G.add_edge(30,15,weight=k)
+G.add_edge(31,14,weight=k)
+G.add_edge(32,13,weight=k)
+G.add_edge(33,12,weight=k)
+G.add_edge(34,11,weight=k)
+G.add_edge(35,10,weight=k)
+
+weight2 = nx.get_edge_attributes(G,'weight')
+pos2 = nx.get_node_attributes(G, 'pos')
+nx.draw_networkx(G,pos2)
+nx.draw_networkx_edge_labels(G,pos2,edge_labels = weight2)
+plt.show()
