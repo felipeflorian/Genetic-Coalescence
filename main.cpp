7a08ef84a4c254810a1e9ed8ab00f0b10ede8460
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-11-24 10:54:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,17 +10,11 @@ int main(){
   int u = 1000;
   for(int i = 1; i < u; i++){change.push_back(i);}
   test.set_population(change);
-
-
   test.nodes();
-  //test.do_simulation();
   vector<int> pob = test.get_population();
-  //for(unsigned int u = 0; u < pob.size(); u++){cout << pob[u] << " ";}
   cout << endl;
-  //test.doing_simulations(3);
   test.doing_simulations(100);
   test.display_tree();
-  //lo hace 5 veces
   test.create_txt();
   test.create_txtcount();
   ofstream iterations;
